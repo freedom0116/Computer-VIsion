720fa5b5f6b38a576a8d3f86235f3293333b93f9
freedom0116 <b04505016@ntu.edu.tw>
2019-11-16 12:24:33 +0800

    hw7

--- hw7/hw7.py
+++ hw7/hw7.py
@@ -71,10 +71,146 @@ def Yokoi(refarr, output):
                 if r == 4:
                     output[y][x] = '5'
             else:
-                output[y][x] = ' '                
-            print(output[y][x], end = '')
+                output[y][x] = ' '
+
+def CharYokoi(refarr, output):
+    size = len(refarr[0]), len(refarr)
+    for y in range(size[1]):
+        for x in range(size[0]):
+            if(refarr[y][x] != ' '):
+                q, r = (0, 0)
+                if((x + 1) < 64 and refarr[y][x + 1] != ' '):
+                    if((y - 1) < 0):
+                        q += 1
+                    else:
+                        if(refarr[y - 1][x + 1] != ' ' and refarr[y - 1][x] != ' '):
+                            r += 1
+                        else:
+                            q += 1
+                if((y - 1) >= 0 and refarr[y - 1][x] != ' '):
+                    if((x - 1) < 0):
+                        q += 1
+                    else:
+                        if(refarr[y - 1][x - 1] != ' ' and refarr[y][x - 1] != ' '):
+                            r += 1
+                        else:
+                            q += 1
+                if((x - 1) >= 0 and refarr[y][x - 1] != ' '):
+                    if((y + 1) > 63):
+                        q += 1
+                    else:
+                        if(refarr[y + 1][x - 1] != ' ' and refarr[y + 1][x] != ' '):
+                            r += 1
+                        else:
+                            q += 1
+                if((y + 1) < 64 and refarr[y + 1][x] != ' '):
+                    if((x + 1) > 63):
+                        q += 1
+                    else:
+                        if(refarr[y + 1][x + 1] != ' ' and refarr[y][x + 1] != ' '):
+                            r += 1
+                        else:
+                            q += 1
+                if q == 1:
+                    output[y][x] = '1'
+                if q == 2:
+                    output[y][x] = '2'
+                if q == 3:
+                    output[y][x] = '3'
+                if q == 4:
+                    output[y][x] = '4'
+                if r == 4:
+                    output[y][x] = '5'
+            else:
+                output[y][x] = ' '
+
+# Pair relationship Operator
+def Pair(refarr, output):
+    size = len(refarr[0]), len(refarr)
+    for y in range(size[1]):
+        for x in range(size[0]):
+            count = 0
+            if(refarr[y][x] == '1'):
+                if((x + 1) < 64 and refarr[y][x + 1] == '1'):
+                    count += 1
+                if((y - 1) >= 0 and refarr[y - 1][x] == '1'):
+                    count += 1
+                if((x - 1) >= 0 and refarr[y][x - 1] == '1'):
+                    count += 1
+                if((y + 1) < 64 and refarr[y + 1][x] == '1'):
+                    count += 1
+                if count < 1:
+                    output[y][x] = 'q'
+                else:
+                    output[y][x] = 'p'
+            else:
+                if(refarr[y][x] != ' '):
+                    output[y][x] = 'q'                 
+        #     print(pairarr[y][x], end = '')
+        # print()
+
+# Connected Shrink Operator
+def Shrink(refarr, output):
+    change = False
+    size = len(refarr[0]), len(refarr)
+    for y in range(size[1]):
+        for x in range(size[0]):
+            count = 0      
+            if output[y][x] == 'p':      
+                count = 0          
+                if((x + 1) < 64 and output[y][x + 1] != ' '):
+                    if((y - 1) < 0):
+                        count += 1
+                    else:
+                        if(output[y - 1][x + 1] == ' ' or output[y - 1][x] == ' '):
+                            count += 1
+                if((y - 1) >= 0 and output[y - 1][x] != ' '):
+                    if((x - 1) < 0):
+                        count += 1
+                    else:
+                        if(output[y - 1][x - 1] == ' ' or output[y][x - 1] == ' '):
+                            count += 1
+                if((x - 1) >= 0 and output[y][x - 1] != ' '):
+                    if((y + 1) > 63):
+                        count += 1
+                    else:
+                        if(output[y + 1][x - 1] == ' ' or output[y + 1][x] == ' '):
+                            count += 1
+                if((y + 1) < 64 and output[y + 1][x] != ' '):
+                    if((x + 1) > 63):
+                        count += 1
+                    else:
+                        if(output[y + 1][x + 1] == ' ' or output[y][x + 1] == ' '):
+                            count += 1
+                if count == 1:
+                    change = True
+                    output[y][x] = ' '
+    return change
+
+# Thinning
+def Thinning(refarr, output):
+    size = len(refarr[0]), len(refarr)
+    pairarr = [[' ' for i in range(64)] for j in range(64)]
+
+    while True:
+        Pair(refarr, pairarr)    
+        if Shrink(refarr, pairarr) == True:
+            CharYokoi(pairarr, refarr)
+        else:
+            break
+
+    for y in range(size[1]):
+        for x in range(size[0]):
+            if pairarr[y][x] != ' ':
+                output[y][x] = 255
+            else:
+                output[y][x] = 0     
+            print(pairarr[y][x], end = '')
         print()
-            
+
+
+
+                
             
 
 
@@ -84,9 +220,12 @@ def Yokoi(refarr, output):
 downarr = np.zeros([64, 64])
 DownSampling(downarr)
 
-# plt.imshow(downarr, cmap = "gray"), plt.title("Downsampling")
-# plt.show()
-
 # Yokoi
 yokoiarr = [[' ' for i in range(64)] for j in range(64)]
 Yokoi(downarr, yokoiarr)
+
+# Thinning
+thinarr = np.zeros([64, 64])
+Thinning(yokoiarr, thinarr)
+plt.imshow(thinarr, cmap = "gray"), plt.title("Thinning")
+plt.show()
